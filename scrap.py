63c0cdfbb0ecbc2d9d08e30a6c2562312189e735
Frank Hsieh <frankhsieh0629@gmail.com>
2017-10-15 16:12:42 +0800

    Add get some tags code

--- scrap.py
+++ scrap.py
@@ -2,9 +2,13 @@ import urllib.request
 import urllib.error
 from bs4 import BeautifulSoup
 
+interest_filter = ["north korea", "baseball", "basketball", "china"]
+
+
 def get_html(url):
     try:
          html = urllib.request.urlopen(url)
+         html.encoding = 'utf8'
     except HTTPError as e:
         return None
     try:
@@ -13,6 +17,31 @@ def get_html(url):
         return None
     return bsObj
 
+#get html info
 obj = get_html('https://www.nytimes.com/')
 
-print("Done")
+#judge if error ro not?
+if obj == None:
+    print("Error happen")
+else:
+    print("HTML found !")
+
+#get html title
+title = obj.head.title.get_text()
+#print(title)
+
+#Sub section 
+sub_sec = obj.find("ul", {"class":"mini-navigation-menu"})
+print(set(sub_sec.get_text()))
+
+story_list = obj.find_all("h2", {"class":"story-heading"})
+
+f = open('A.txt', 'w', encoding = 'UTF-8')
+
+for name in story_list:
+    f.write(name.get_text())
+    f.write('\n')
+f.close()
+
+#title = get_title(obj)
+#print(title)
