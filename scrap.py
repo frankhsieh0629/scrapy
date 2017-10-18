13701070430e1b85e05e68ae4a65358d8610d952
Frank Hsieh <frankhsieh0629@gmail.com>
2017-10-18 23:17:03 +0800

    Can catch story title and link.

--- scrap.py
+++ scrap.py
@@ -1,9 +1,10 @@
+# coding:utf-8
+
 import urllib.request
 import urllib.error
 from bs4 import BeautifulSoup
 
 interest_filter = ["north korea", "baseball", "basketball", "china"]
-
 
 def get_html(url):
     try:
@@ -32,16 +33,32 @@ title = obj.head.title.get_text()
 
 #Sub section 
 sub_sec = obj.find("ul", {"class":"mini-navigation-menu"})
-print(set(sub_sec.get_text()))
+#print(set(sub_sec.get_text()))
 
-story_list = obj.find_all("h2", {"class":"story-heading"})
+story_lists = obj.find_all("h2", {"class":"story-heading"})
+#story_link = story_list.("href")
+#print(story_list)
 
-f = open('A.txt', 'w', encoding = 'UTF-8')
+for story in story_lists:
+    try:
+        story_title = story.find('a').get_text()
+        for story_link in story.find_all('a', href=True):
+            print(story_title)
+            print(story_link['href'])
+    except UnicodeEncodeError:
+        pass
+    except AttributeError:
+        pass
+
+
+"""
+f = open('BBB.txt', 'w', encoding = 'UTF-8')
 
 for name in story_list:
     f.write(name.get_text())
     f.write('\n')
 f.close()
-
+print("QQQ")
 #title = get_title(obj)
 #print(title)
+"""
